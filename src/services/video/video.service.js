3c3ab7cd51f8ee7b9d6900d7449a728e63a7319f
MyronRigel <myron.rigel@gmail.com>
2022-02-09 16:44:09 +0200

    added eslint

--- src/services/video/video.service.js
+++ src/services/video/video.service.js
@@ -8,37 +8,38 @@ class VideoService {
   async addVideo({files}) {
     try {
       const name = `${uuid()}.${files.video.name}`
-      const writableStream = fs.createWriteStream(path.join(process.cwd(), 'src', 'video', `${name}`))
+      const writableStream = fs.createWriteStream(path.join(process.cwd(), 'src', 'video', name))
 
-      writableStream.on('error', error => new ErrorHandler('error while writing video on server', 500))
+      writableStream.on('error', error => new ErrorHandler(error.message, 500))
       writableStream.write(files.video.data)
 
       return name
-
     } catch (error) {
-      throw new ErrorHandler('error while writing video on server', 500)
+      return new ErrorHandler('error while writing video on server', 500)
     }
   }
 
   async getVideoById({params: {id}}, res) {
     try {
       const readableStream = fs.createReadStream(path.join(process.cwd(), 'src', 'video', id))
-      readableStream.on('error', error => new ErrorHandler('error while writing video on server', 500))
+      readableStream.on('error', error => new ErrorHandler(error.message, 500))
 
       res.header('Cross-Origin-Resource-Policy', 'cross-origin')
       readableStream.pipe(res)
 
     } catch (error) {
-      throw new ErrorHandler('error while writing video on server', 500)
+      return new ErrorHandler(error.message, 500)
     }
   }
 
   async getAllVideos() {
     try {
       const names = fs.readdirSync(path.join(process.cwd(), 'src', 'video'))
+
       return names
+
     } catch (error) {
-      throw new ErrorHandler('error while writing video on server', 500)
+      return new ErrorHandler(error.message, 500)
     }
   }
 
